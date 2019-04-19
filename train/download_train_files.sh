7974aaba183678b4e01051b4389f343f46ba361a
hildekuehne <hildekuehne@users.noreply.github.com>
2019-04-20 00:30:59 +0200

    Create download_train_files.sh

---
+++ train/download_train_files.sh
@@ -0,0 +1,23 @@
+
+## segmentation (76 MB)
+# https://drive.google.com/file/d/1JfvbwdfEqV8aHQ-E87_vtyaZeSCiGkTo
+
+## clips (in .mp4):
+# https://drive.google.com/file/d/1imWhxixFEB7V4ASJ3tXYpoBZ8hA6ht00
+
+## features in numpy format
+## rgb1 70 GB:
+# wget https://drive.google.com/file/d/13zo0UPqlruutKEkKS0g6QEuKNt-Hi0uf
+## rgb2 55 GB:
+## wget https://drive.google.com/file/d/1_PCjQ0LKjzSB_v53AzFl-yzDTVDZ_HTq
+## flow1 76 GB:
+# wget https://drive.google.com/file/d/1VG9oaTZ00z2xbCtSy240eKstCNGXKwV6
+## flow2 66 GB:
+## wget https://drive.google.com/file/d/1E9AJngHFUlcuotaZ6dr5t6mUX9-X0aPf
+
+## features in numpy format, pre packed in chuncks of 1000 clips for easier loading:
+## part1 :
+# wget https://drive.google.com/file/d/1xRAnz8Okrl4NDaXJiQQPFAeWuiGFZury
+## part2 :
+# wget https://drive.google.com/file/d/15S8rbf7YDjU83rqCidfPVbDQAYJzOnwP
+
