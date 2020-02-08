2b57433e39708c53a7cbc4e4540454498ecdc233
hildekuehne <hildekuehne@gmx.de>
2020-02-07 19:21:28 -0500

    Download links fixed

--- test/download_test_files.sh
+++ test/download_test_files.sh
@@ -6,9 +6,7 @@ echo 'Please uncomment the files you want to download ... '
 ## all test data - data_test.tar.gz
 ## includes full videos, features, transcripts, and groundtruht
 ################
-# FILE_ID = "1wmJvkvqZoqsD8rvFVZ3KLV2vV9WXv89R"
-# wget --load-cookies ./cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies ./cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=$FILE_ID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILE_ID" -O data_test.tar.gz && rm -rf ./cookies.txt
-# mkdir ./data_test
-# tar xzf data_test.tar.gz -C ./data_test
+# wget --load-cookies ./cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies ./cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1wmJvkvqZoqsD8rvFVZ3KLV2vV9WXv89R' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1wmJvkvqZoqsD8rvFVZ3KLV2vV9WXv89R" -O data_test.tar.gz && rm -rf ./cookies.txt
+# tar xzf data_test.tar.gz
 
 echo 'DONE!'
